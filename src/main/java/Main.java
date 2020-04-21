45c6b3ffe6df1ae12602ecaf56ea8d5c701662a7
Daniel Duczymiński <215710@edu.p.lodz.pl>
2020-04-21 16:33:31 +0200

    add simple database
    added simple database, connection established, printing table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,65 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
 import static spark.Spark.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static String getDataFromDB() throws URISyntaxException, SQLException {
+        Connection conn = getConnection();
+        Statement statement = conn.createStatement();
+        String query = "SELECT * from public.\"Produkty\";";
+        ResultSet result = statement.executeQuery(query);
+        String line = "";
+        while(result.next())
+        {
+            line = line + result.getInt("id") + " " + result.getString("nazwa") + " "
+                    + result.getDouble("cena")+ " " + result.getString("kategoria") +"\n";
+            System.out.println(line);
+        }
+        return line;
+/*
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("/src/hibernate.cfg.xml").applySettings(
+                configuration.getProperties()).build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(new Produkt(10,"Banan", 0.6,"Owoce"));
+        session.getTransaction().commit();
+*/
+
+    }
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    public static void main(String[] args) throws URISyntaxException, SQLException {
+
         port(getHerokuAssignedPort());
 
-        get("/hello", (req, res) -> "Hello Heroku World");
-        get("/", (req, res) -> "pierwsza strona aby odblokowac druga dodaj /hello");
+        get("/", (req, res) -> getDataFromDB());
+
+        get("/hello", (req, res) -> "Siemanko");
+        //get("/", (req, res) -> "pierwsza strona aby odblokowac druga dodaj /hello");
+        //System.out.println(getNameFromDB());
+
+
+
     }
 
     static int getHerokuAssignedPort() {
---
+++ src/main/java/Produkt.java
@@ -0,0 +1,60 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Produkt
+{
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private int id;
+
+    @Column(name="nazwa", nullable = false)
+    private String nazwa;
+
+    @Column(name="cena", nullable = false)
+    private double cena;
+
+    @Column(name="kategoria", nullable = false)
+    private String kategoria;
+
+    Produkt(int id, String nazwa, double cena, String kategoria)
+    {
+        this.id = id;
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.kategoria = kategoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+}
