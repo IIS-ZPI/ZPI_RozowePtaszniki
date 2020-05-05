f55a745ffc39fd63a8bfe8152bc9cad1f50ec370
mati9844 <mateuszdomalazek@interia.pl>
2020-05-05 23:47:21 +0200

    Create ProfitCalculatorTest.java

---
+++ src/main/java/app/Tests/ProfitCalculatorTest.java
@@ -0,0 +1,12 @@
+package app.Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfitCalculatorTest {
+
+    @Test
+    void calculate() {
+    }
+}
