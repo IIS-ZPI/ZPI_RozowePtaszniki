9365f08de6b3b734f76a150bbc37511c68ff31e6
Leonard Wojtczak <medyk170@gmail.com>
2020-05-18 17:02:31 +0200

    Update ProfitCalculatorText

--- src/main/java/app/Tests/ProfitCalculatorTest.java
+++ src/main/java/app/Tests/ProfitCalculatorTest.java
@@ -15,11 +15,10 @@ class ProfitCalculatorTest {
         double finalPrice = 10;
         double finalPriceWithoutTaxes = Math.round(finalPrice / (1.0 + baseTax/100.0) * 100.0) / 100.0;
 
-        ProfitData p1 = new ProfitData("Alabama", 10, "prescriptionDrug", 5.01);
+        ProfitData p1 = new ProfitData("Alabama", 10, 5.01);
 
         assertEquals(p1.getNameOfState(), "Alabama");
         assertEquals(p1.getPriceWithoutTaxes(), finalPriceWithoutTaxes);
-        assertEquals(p1.getCategory(), "prescriptionDrug");
         assertEquals(p1.getProfit(), (finalPriceWithoutTaxes-startingPrice));
 
 
@@ -33,11 +32,10 @@ class ProfitCalculatorTest {
         double finalPrice = 50;
         double finalPriceWithoutTaxes = Math.round(finalPrice / (1.0 + baseTax/100.0) * 100.0) / 100.0;
 
-        ProfitData p1 = new ProfitData("California", 50, "prescriptionDrug", 45.01);
+        ProfitData p1 = new ProfitData("California", 50,45.01);
 
         assertEquals(p1.getNameOfState(), "California");
         assertEquals(p1.getPriceWithoutTaxes(), finalPriceWithoutTaxes);
-        assertEquals(p1.getCategory(), "prescriptionDrug");
         assertEquals(p1.getProfit(), (finalPriceWithoutTaxes-startingPrice));
 
 
@@ -51,11 +49,10 @@ class ProfitCalculatorTest {
         double finalPrice = 8;
         double finalPriceWithoutTaxes = Math.round(finalPrice / (1.0 + baseTax/100.0) * 100.0) / 100.0;
 
-        ProfitData p1 = new ProfitData("Alabama", 7.69, "groceries", 7.49);
+        ProfitData p1 = new ProfitData("Alabama", 7.69,7.49);
 
         assertEquals(p1.getNameOfState(), "Alabama");
         assertEquals(p1.getPriceWithoutTaxes(), finalPriceWithoutTaxes);
-        assertEquals(p1.getCategory(), "groceries");
         assertEquals(p1.getProfit(), (finalPriceWithoutTaxes-startingPrice));
 
 
@@ -69,11 +66,10 @@ class ProfitCalculatorTest {
         double finalPrice = 10;
         double finalPriceWithoutTaxes = Math.round(finalPrice / (1.0 + baseTax/100.0) * 100.0) / 100.0;
 
-        ProfitData p1 = new ProfitData("Alabama", 10, "groceries", 5.01);
+        ProfitData p1 = new ProfitData("Alabama", 10, 5.01);
 
         assertEquals(p1.getNameOfState(), "Alabama");
         assertEquals(p1.getPriceWithoutTaxes(), finalPriceWithoutTaxes);
-        assertEquals(p1.getCategory(), "groceries");
         assertEquals(p1.getProfit(), (finalPriceWithoutTaxes-startingPrice));
 
 
