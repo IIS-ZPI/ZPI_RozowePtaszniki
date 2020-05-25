819ac25eb2f7fed38a1ca0846487e7c012ccb89d
mati9844 <mateuszdomalazek@interia.pl>
2020-05-25 16:43:40 +0200

    Add products from csv file
    add 65 products from csv file into array list of products. Maybe exist problem with id. We should create/use method nextId()

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,6 +5,9 @@ import com.google.gson.Gson;
 import spark.Spark;
 import spark.utils.IOUtils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,7 +18,7 @@ import static spark.Spark.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException, SQLException {
         ProfitCalculator profitCalculator = new ProfitCalculator();
         //System.out.println(profitCalculator.CalculateForAllStates("Jabłko 0.2 groceries 10"));
 
@@ -31,6 +34,26 @@ public class Main {
             String dumbString = req.params(":product") + " " + req.params(":cost") + " " + req.params(":category") + " " + req.params(":final_cost"); // this needs to be changed @Leonard
             return profitCalculator.CalculateForAllStates(dumbString);
         });
+
+        /*Load products from csv file to products ArrayList*/
+        ArrayList<Product> products = null;
+        System.out.println("Products from csv file: ");
+        try {
+            products = csvReader("products.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (products != null)
+            for (Product product : products) {
+                System.out.println(product.getId() + "," + product.getNazwa() + "," + product.getCena() + "," + product.getKategoria());
+            }
+
+        /*Adding products list from csv to DB - maybe exist problem with id (look method csvReader()) */
+        for (Product product : products) {
+            //putProductIntoDB(product);
+
+        }
     }
 
     private static String renderContent(String htmlFile) {
@@ -71,6 +94,35 @@ public class Main {
             products.add(new Product(result.getInt("id"),result.getString("nazwa"),
                     result.getDouble("cena"),result.getString("kategoria")));
         }
+        return products;
+    }
+
+    static ArrayList <Product> csvReader(String path) throws IOException {
+        ArrayList <Product> products = new ArrayList<>();
+        String row;
+
+        int id = 11; //nextid from db
+
+        BufferedReader csvReader = new BufferedReader(new FileReader(path));
+        while ((row = csvReader.readLine()) != null) {
+            Product product = new Product(0,"nazwa",0.00,"kategoria");
+
+            String[] data = row.split(";");
+            product.setId(id);
+            product.setNazwa(data[0]);
+            product.setCena(Double.parseDouble(data[1]));
+            if(data[2] == "groceries" ||
+                    data[2] == "preparedFood" ||
+                    data[2] == "prescriptionDrug" ||
+                    data[2] == "nonPrescriptionDrug" ||
+                    data[2] == "clothing" ||
+                    data[2] == "intangibles");
+            product.setKategoria(data[2]);
+            id++;
+            products.add(product);
+        }
+
+        csvReader.close();
         return products;
     }
 
